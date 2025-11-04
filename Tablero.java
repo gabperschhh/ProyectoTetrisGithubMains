736014b0fb9c6cfd7e84f4d20ff9f920be7cc67f
Gabriel Perez <gabpersch@gmail.com>
2025-11-03 20:30:18 -0600

    Variable celdas y metodos imprimir e inicializar

--- Tablero.java
+++ Tablero.java
@@ -1,20 +1,28 @@
-public class Tablero{
+public class Tablero {
     private int ancho;
     private int alto;
-    public Tablero(int ancho, int alto){
+    private int[][] celdas;
+
+    public Tablero(int ancho, int alto) {
         this.ancho = ancho;
         this.alto = alto;
+        this.celdas = new int[alto][ancho];
     }
-    public int getAncho(){
-        return ancho;
+
+    public void inicializar() {
+        for (int i = 0; i < alto; i++) {
+            for (int j = 0; j < ancho; j++) {
+                celdas[i][j] = 0;
+            }
+        }
     }
-    public String getAlto(){
-        return alto;
-    }
-    public void setAncho(int ancho){
-        this.ancho = ancho;
-    }
-    public void setAlto(int alto){
-        this.alto = alto;
+
+    public void imprimir() {
+        for (int i = 0; i < alto; i++) {
+            for (int j = 0; j < ancho; j++) {
+                System.out.print(celdas[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
