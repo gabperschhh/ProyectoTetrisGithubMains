15c8476007186645162fde1e18981dec8b03ec8d
Gabriel Perez <gabpersch@gmail.com>
2025-11-05 00:11:45 -0600

    Several changes

--- Pieza.java
+++ Pieza.java
@@ -1,28 +1,146 @@
 public class Pieza{
-    private Bloque bloque;
-    private String color;
+    private Bloque[] bloques;
     private int[][] forma;
-    public Pieza(Bloque bloque, String color, int[][] forma){
-        this.bloque = bloque;
-        this.color = color;
+
+    public Pieza(Bloque[] bloques,  int[][] forma){
+        this.bloques = bloques;
         this.forma = forma;
     }
-    public Bloque getBloque(){
-        return bloque;
+
+    public Bloque[] getBloques(){
+        return bloques;
     }
-    public String getColor(){
-        return color;
-    }
+
     public int[][] getForma(){
         return forma;
     }
-    public void setBloque(Bloque bloque){
-        this.bloque = bloque;
+
+    public void setBloques(Bloque[] bloques){
+        this.bloques = bloques;
     }
-    public void setColor(String color){
-        this.color = color;
-    }
+
+
     public void setForma(int[][] forma){
         this.forma = forma;
+    }
+
+    public int generarRandom(){
+        int randomNum = (int)(Math.random() * 7) + 1;
+        return randomNum;
+    }
+
+    public int[][] generarForma(){
+        int idPieza = generarRandom();
+        int[][] p;
+
+        switch (idPieza) {
+            case 1 -> {
+                p = new int[][] {{1}, {1}, {1}, {1}};
+            }
+            case 2 -> {
+                p = new int[][] {{1 , 1}, {1 , 1}};
+            }
+            case 3 -> {
+                p = new int[][] {{0, 1, 1}, {1, 1, 0}};
+            }
+            case 4 -> {
+                p = new int[][] {{0, 1, 0}, {1, 1, 1}};
+            }
+            case 5 -> {
+                p = new int[][] {{1, 1, 0}, {0, 1, 1}};
+            }
+            case 6 -> {
+                p = new int[][] {{1, 0}, {1, 0}, {1, 0}, {1, 1}};
+            }
+            case 7 -> {
+                p = new int[][] {{0, 1}, {0, 1}, {0, 1}, {1, 1}};
+            }
+            default -> {
+                p = null;
+            }
+        }
+
+        this.forma = p;   
+        return p;
+    }
+
+    public String generarColor(){
+        int idColor = generarRandom();
+        switch (idColor) {
+            case 1 -> {
+                String azul = "azul";
+                return azul;
+            }
+            case 2 -> {
+                String rojo = "rojo";
+                return rojo;
+            }
+            case 3 -> {
+                String verde = "verde";
+                return verde;
+            }
+            case 4 -> {
+                String naranja = "naranja";
+                return naranja;
+            }
+            case 5 -> {
+                String amarillo = "amarillo";
+                return amarillo;
+            }
+            case 6 -> {
+                String rosado = "rosado";
+                return rosado;
+            }
+            case 7 -> {
+                String morado = "morado";
+                return morado;
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
+    public Bloque[] generarBloques(){
+        if (this.forma == null) {
+            // por seguridad, genera la forma si aún no está
+            generarForma();
+        }
+        int contador = 0; //cuenta cuantos "1" hay en la forma
+        for (int i = 0; i < forma.length; i++) {           
+            for (int j = 0; j < forma[i].length; j++) { 
+                if (forma[i][j] == 1) {
+                    contador++;
+                }
+            }
+        }
+
+        
+        Bloque[] bloques = new Bloque[contador]; //crea el arreglo segun el contador
+        int k = 0;
+
+        
+        for (int i = 0; i < forma.length; i++) { //asigna los bloques segun la forma
+            for (int j = 0; j < forma[i].length; j++) {
+                if (forma[i][j] == 1) {
+                    int[] coords = {j, i}; 
+                    bloques[k] = new Bloque(coords, generarColor(), coords[0], coords[1]);
+                    k++;
+                }
+            }
+        }
+
+        this.bloques = bloques;
+        return bloques;
+    }   
+
+    public void imprimirForma(){ // para tests
+        int[][] pieza = generarForma();
+        for(int i = 0; i < pieza.length; i++){
+            for(int j = 0; j < pieza[i].length; j++){
+                System.out.print(pieza[i][j]);
+            }
+            System.out.println();
+        }
     }
 } 
--- Tablero.java
+++ Tablero.java
@@ -1,12 +1,24 @@
 public class Tablero {
-    private int ancho;
     private int alto;
+    private int ancho;
     private int[][] celdas;
 
-    public Tablero(int ancho, int alto) {
-        this.ancho = ancho;
+    public Tablero(int alto, int ancho) {
         this.alto = alto;
+        this.ancho = ancho;
         this.celdas = new int[alto][ancho];
+    }
+
+    public int getAlto(){
+        return alto;
+    }
+
+    public int getAncho(){
+        return ancho;
+    }
+
+    public int[][] getCeldas(){
+        return celdas;
     }
 
     public void inicializar() {
@@ -17,12 +29,40 @@ public class Tablero {
         }
     }
 
-    public void imprimir() {
+    public void imprimir(Pieza piezaActual) {
         for (int i = 0; i < alto; i++) {
+            System.out.print("|");
             for (int j = 0; j < ancho; j++) {
-                System.out.print(celdas[i][j] + " ");
+                boolean esBloqueDePieza = false;
+
+                if (piezaActual != null) {
+                    for (Bloque b : piezaActual.getBloques()) {
+                        int[] c = b.getCoords();
+                        if(c[0] == j && c[1] == i){
+                            esBloqueDePieza = true;
+                            break;
+                        }
+                    }
+                }
+
+                if(esBloqueDePieza){
+                    System.out.print("X | ");
+                } else {
+                    System.out.print(celdas[i][j] + " | ");
+                }
+                
             }
             System.out.println();
         }
     }
+
+    public void fijarPieza(Pieza piezaActual) {
+        for (Bloque b : piezaActual.getBloques()) {
+            int[] c = b.getCoords();
+            int x = c[0];
+            int y = c[1];
+
+            celdas[y][x] = 1; 
+        }
+}
 }
