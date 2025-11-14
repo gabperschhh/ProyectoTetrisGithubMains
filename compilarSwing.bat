a654bbce3df8b7cfcf88a9a0cafaf0937d1202aa
Gabriel Perez <gabpersch@gmail.com>
2025-11-14 17:42:46 -0600

    Compilador grafico

---
+++ compilarSwing.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Compilando archivos Java...
+javac *.java
+
+if %errorlevel% neq 0 (
+    echo Error en la compilación.
+    pause
+    exit /b
+)
+
+echo Ejecutando programa...
+java Pantalla
+
+pause
